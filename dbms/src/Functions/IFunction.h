078ed617c18b9ccda967338142629ffbc6c54912
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2018-02-06 22:58:31 +0300

    removed prerequisites [#CLICKHOUSE-3550]

--- dbms/src/Functions/IFunction.h
+++ dbms/src/Functions/IFunction.h
@@ -274,7 +274,7 @@ class DefaultExecutable final : public PreparedFunctionImpl
 public:
     explicit DefaultExecutable(std::shared_ptr<IFunction> function) : function(std::move(function)) {}
 
-    String getName() const override { return function->getName() };
+    String getName() const override { return function->getName(); }
 
 protected:
     void executeImpl(Block & block, const ColumnNumbers & arguments, size_t result) final
